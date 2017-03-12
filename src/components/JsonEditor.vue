b04c5ba843e3469971bc0cc742eb70864a8c2e3f
dirkliu <liuqi0702@126.com>
2017-03-12 13:24:01 +0800

    初步完成vue-jsoneditor

---
+++ src/components/JsonEditor.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="json-editor-container">
+    <div class="tree-mode"></div>
+    <div class="code-mode"></div>
+  </div>
+</template>
+
+<script>
+  import 'jsoneditor/dist/jsoneditor.css'
+  import JsonEditor from 'jsoneditor/dist/jsoneditor.min'
+
+  export default {
+    mounted () {
+      var json = {
+        name: 'liuqi',
+        age: '32'
+      }
+      this.treeEditor = new JsonEditor(this.$el.getElementsByClassName('tree-mode')[0])
+      this.treeEditor.set(json)
+
+      this.codeEditor = new JsonEditor(this.$el.getElementsByClassName('code-mode')[0], {
+        'mode': 'code'
+      })
+      this.codeEditor.set(json)
+
+      // this.treeEditor.get()
+    }
+  }
+</script>
+
+<style>
+  .ace_line_group {
+    text-align: left;
+  }
+
+  .json-editor-container {
+    display: flex;
+    width: 100%;
+  }
+
+  .json-editor-container .tree-mode {
+    width: 50%;
+  }
+
+  .json-editor-container .code-mode {
+    flex-grow: 1;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+// import 'jsoneditor/dist/img/jsoneditor-icons.svg'
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
