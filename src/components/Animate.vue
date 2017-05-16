1b987cbe7aa1f7a1ceb06d73c9ace70d78bed629
dirkliu <liuqi0702@126.com>
2017-05-16 20:21:53 +0800

    transiton exercise

--- src/components/Animate.vue
+++ src/components/Animate.vue
@@ -1,8 +1,28 @@
 <template>
-  <div>在此写一些效果</div>
+  <div id="demo">
+    <h2><a href="https://cn.vuejs.org/v2/guide/transitions.html" target="_blank">vue过渡状态</a></h2>
+    <button v-on:click="show = !show">transition</button>
+    <transition name="fade">
+      <p v-if="show">opacity</p>
+    </transition>
+  </div>
 </template>
 
 <script>
   export default {
+    data () {
+      return {
+        show: false
+      }
+    }
   }
 </script>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .9s;
+}
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
+</style>
