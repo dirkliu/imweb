9f8108ed7ecd3ea6f437c3871d0d1b2137e7486d
dirkliu <liuqi0702@126.com>
2017-06-07 23:04:44 +0800

    event show

--- src/components/Drag.vue
+++ src/components/Drag.vue
@@ -2,17 +2,20 @@
 <div class="drag">
   <h2>拖放实验</h2>
   <ul>
-    <draggable v-model="tasks">
-      <transition-group>
-        <li v-for="(task, index) in tasks" :key="index" :id="index" :style="{'background-position': 'center', 'background-image': 'url('+ task.img +')'}">
-          <input type="checkbox" name="completed[]" value="1" v-bind:checked="task.completed">
-          <strong>{{ task.title }}</strong>
-        </li>
-      </transition-group>
+    <draggable
+      v-model="tasks"
+      element="ul"
+      @start="onStart"
+      @choose="onChoose"
+      @sort="onSort"
+      @update="onUpdate"
+      @end="onEnd">
+      <li v-for="(task, index) in tasks" :key="index" :id="index" :style="{'background-position': 'center', 'background-image': 'url('+ task.img +')'}">
+        <input type="checkbox" name="completed[]" value="1" v-bind:checked="task.completed">
+        <strong>{{ task.title }}</strong>
+      </li>
     </draggable>
   </ul>
-
-
 </div>
 </template>
 
@@ -32,52 +35,34 @@ export default {
         'title': 'Task 1',
         'completed': true,
         img: 'https://ps.ssl.qhimg.com/sdmt/183_135_100/t01f0341330ab8ad809.jpg'
-      }, {
-        'id': 24,
-        'title': 'Task 2',
-        'completed': false,
-        img: 'https://ps.ssl.qhimg.com/sdmt/183_135_100/t01f0341330ab8ad809.jpg'
-      }, {
-        'id': 36,
-        'title': 'Task 3',
-        'completed': true,
-        img: 'https://ps.ssl.qhimg.com/sdmt/182_135_100/t01fb01d829f489595f.jpg'
-      }, {
-        'id': 48,
-        'title': 'Task 4',
-        'completed': false,
-        img: 'http://p5.so.qhimgs1.com/bdr/_240_/t0102d8f5b3fb0bee5b.jpg'
-      }, {
-        'id': 60,
-        'title': 'Task 5',
-        'completed': true,
-        img: 'http://p1.so.qhmsg.com/bdr/_240_/t015b68d918d6833786.jpg'
-      }, {
-        'id': 72,
-        'title': 'Task 6',
-        'completed': false,
-        img: 'http://p4.so.qhimgs1.com/bdr/_240_/t013402563551e8b74a.jpg'
-      }, {
-        'id': 84,
-        'title': 'Task 7',
-        'completed': true,
-        img: 'http://p5.so.qhimgs1.com/bdr/_240_/t01cc9d2315ff47684b.jpg'
-      }, {
-        'id': 0,
-        'title': 'Task 0',
-        'completed': false,
-        img: 'http://p3.so.qhimgs1.com/bdr/_240_/t01b62b0c70afce5f33.gif'
-      }, {
-        'id': 108,
-        'title': 'Task 9',
-        'completed': true,
-        img: 'http://p5.so.qhimgs1.com/bdr/_240_/t01b727795ea572c24b.jpg'
       }]
     }
   },
 
   components: {
     draggable
+  },
+
+  methods: {
+    onStart (e) {
+      console.log('onStart:', e)
+    },
+
+    onSort (e) {
+      console.log('onSort:', e)
+    },
+
+    onUpdate (e) {
+      console.log('onUpdate:', e)
+    },
+
+    onEnd (e) {
+      console.log('onEnd:', e)
+    },
+
+    onChoose (e) {
+      console.log('onChoose:', e)
+    }
   }
 }
 </script>
