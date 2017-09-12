a2afe775f3e955dff7423f2f210a5d7d3cc5e633
dirkliu <liuqi0702@126.com>
2017-09-12 23:36:40 +0800

    nested modal!

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="demo-page">
     <h1>dialog演示</h1>
-    <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
+    <p>此例可以证实：element-ui的dialog组件是不能嵌套使用的</p>
+    <el-button type="text" @click="showModal">点击打开 Dialog</el-button>
+    <nested-modal ref="nestedModal"></nested-modal>
   </div>
 </template>
+<script>
+  import NestedModal from './NestedModal.vue'
+  export default {
+    data () {
+      return {}
+    },
+
+    components: {
+      NestedModal
+    },
+
+    methods: {
+      showModal () {
+        this.$refs.nestedModal.init()
+      }
+    }
+  }
+</script>
---
+++ src/components/NestedModal.vue
@@ -0,0 +1,41 @@
+<template>
+<el-dialog title="提示" :visible.sync="visible" size="large" :modal="false" class="bottom-dialog">
+  <h1>第一个dialog</h1>
+  <el-button type="text" @click="showModal">点击打开 Dialog</el-button>
+  <span>这是一段信息</span>
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="visible = false">取 消</el-button>
+    <el-button type="primary" @click="visible = false">确 定</el-button>
+  </span>
+  <nested-up-modal ref="nestedUpModal"></nested-up-modal>
+</el-dialog>
+</template>
+<script>
+import NestedUpModal from './NestedUpModal.vue'
+export default {
+  data () {
+    return {
+      visible: false
+    }
+  },
+
+  components: {
+    NestedUpModal
+  },
+
+  methods: {
+    init () {
+      this.visible = true
+    },
+
+    showModal () {
+      this.$refs.nestedUpModal.init()
+    }
+  }
+}
+</script>
+<style>
+.bottom-dialog{
+  height:50%;
+}
+</style>
