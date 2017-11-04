31087dabee14d81a214779286dac08df563e1944
dirkliu <liuqi0702@126.com>
2017-11-04 21:41:38 +0800

    lifecycle change!

--- src/app/lifecycle/Lifecycle.vue
+++ src/app/lifecycle/Lifecycle.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="lifecycle-page">
   <h2>生命周期演示</h2>
+  <div>beforeCreate之前，是没有data属性的</div>
   <lifecycle-child></lifecycle-child>
 </div>
 </template>
@@ -18,12 +19,25 @@ export default {
     LifecycleChild
   },
 
+  methods: {
+    showTest (title) {
+      console.log('test:' + title)
+    }
+  },
+
+  beforeCreate () {
+    console.log('this beforeCreate:', this)
+    console.log('beforeCreate:', this.title)
+  },
+
   created () {
-    console.log(this.title + ' created')
+    console.log('created:', this.title)
+    this.showTest('created')
   },
 
   mounted () {
-    console.log(this.title + ' mounted')
+    console.log('mounted:', this.title)
+    this.showTest('mounted')
   }
 }
 </script>
